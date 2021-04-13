9e42e7f08f2714ce1c12b5fd3c8a9de1641329a6
Alf Nielsen <alfnielsen@gmail.com>
2021-04-13 08:52:06 +0200

    Update MessageBridgeService.test.ts

--- tests/MessageBridgeService.test.ts
+++ tests/MessageBridgeService.test.ts
@@ -1,8 +1,7 @@
 import WS from "jest-websocket-mock"
 import { Message } from "../src/Message"
 import { MessageBridgeService } from "../src/MessageBridgeService"
-import { MessageDirection } from "../src/MessageDirection"
-import { MessageInterfaces } from "../src/MessageBridgeInterfaces"
+import {MessageDirection, MessageType} from "../src/MessageBridgeInterfaces"
 
 interface TodoItem {
   id: number
@@ -20,7 +19,7 @@ interface UpdateTodoItemCommandDto {
 it("should re-call query on event messages from the server", async () => {
   jest.mock("@microsoft/signalr")
   const wsUri = "ws://localhost:1234"
-  var mbs = new MessageBridgeService(wsUri)
+  let mbs = new MessageBridgeService(wsUri)
   // @ts-ignore (internalSendMessage is a private method!)
   const spy = jest.spyOn(mbs, "internalSendMessage")
 
@@ -30,49 +29,51 @@ it("should re-call query on event messages from the server", async () => {
     name: "UpdateTodoItem",
     query: { nameLike: "test" },
     triggers: ["UpdateTodoItemEvent"],
-    update: (msg) => {},
+    onUpdate: (msg) => {},
   })
 
   expect(mbs.history.length).toBe(1)
   // SERVER:
-  var serverFromMsg = Message.create<TodoItem[]>({
+  let serverFromMsg = Message.create<TodoItem[]>({
     name: "UpdateTodoItem",
-    type: MessageInterfaces.QueryResponse,
+    type: MessageType.QueryResponse,
     direction: MessageDirection.ToClient,
-    payload: [{ id: 1, title: "title1" }],
-  })
+    payload: [{id: 1, title: "title1"}],
+  });
+  //@ts-ignore
   mbs.handleIncomingMessage(serverFromMsg)
   // CLIENT
   expect(mbs.history.length).toBe(2)
   // SERVER:
-  var serverFromMsg = Message.create<TodoItem[]>({
+  serverFromMsg = Message.create<TodoItem[]>({
     name: "UpdateTodoItemEvent",
-    type: MessageInterfaces.Event,
+    type: MessageType.Event,
     direction: MessageDirection.ToClient,
-  })
+  });
   // send (2)
+  //@ts-ignore
   mbs.handleIncomingMessage(serverFromMsg)
   // Should trigger re-call to server
   // CLIENT
   expect(mbs.history.length).toBe(4)
   //
   expect(mbs.history[0].name).toBe("UpdateTodoItem")
-  expect(mbs.history[0].type).toBe(MessageInterfaces.Query)
+  expect(mbs.history[0].type).toBe(MessageType.Query)
   expect(mbs.history[0].payload).toMatchObject({ nameLike: "test" })
   expect(mbs.history[0].direction).toBe(MessageDirection.ToServer)
   //
   expect(mbs.history[1].name).toBe("UpdateTodoItem")
-  expect(mbs.history[1].type).toBe(MessageInterfaces.QueryResponse)
+  expect(mbs.history[1].type).toBe(MessageType.QueryResponse)
   expect(mbs.history[1].payload).toMatchObject([{ id: 1, title: "title1" }])
   expect(mbs.history[1].direction).toBe(MessageDirection.ToClient)
   //
   expect(mbs.history[2].name).toBe("UpdateTodoItemEvent")
-  expect(mbs.history[2].type).toBe(MessageInterfaces.Event)
+  expect(mbs.history[2].type).toBe(MessageType.Event)
   expect(mbs.history[2].payload).toBeUndefined()
   expect(mbs.history[2].direction).toBe(MessageDirection.ToClient)
   //
   expect(mbs.history[3].name).toBe("UpdateTodoItem")
-  expect(mbs.history[3].type).toBe(MessageInterfaces.Query)
+  expect(mbs.history[3].type).toBe(MessageType.Query)
   expect(mbs.history[3].payload).toMatchObject({ nameLike: "test" })
   expect(mbs.history[3].direction).toBe(MessageDirection.ToServer)
   expect(mbs.history[3].trackId).not.toBe("tract_1")
