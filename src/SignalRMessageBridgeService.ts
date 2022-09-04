32c93d9588b903e1936d97a4112d3929b167ea5a
Alf Nielsen <alfnielsen@gmail.com>
2022-09-04 11:51:47 +0200

    add microbundle for build

--- src/SignalRMessageBridgeService.ts
+++ src/SignalRMessageBridgeService.ts
@@ -1,34 +1,32 @@
-import * as signalR from "@microsoft/signalr";
-import { Message } from "./Message";
-import { IHttpConnectionOptions } from "@microsoft/signalr/src/IHttpConnectionOptions";
-import { MessageBridgeServiceBase } from "./MessageBridgeServiceBase";
+import * as signalR from "@microsoft/signalr"
+import { Message } from "./Message"
+import { IHttpConnectionOptions } from "@microsoft/signalr/src/IHttpConnectionOptions"
+import { MessageBridgeServiceBase } from "./MessageBridgeServiceBase"
 
 export class SignalRMessageBridgeService extends MessageBridgeServiceBase {
-  connection?: signalR.HubConnection;
-
   connect(options: IHttpConnectionOptions = {}): Promise<void> {
     this.connection = new signalR.HubConnectionBuilder()
       .withUrl(this.wsUri, options)
       .withAutomaticReconnect()
-      .build();
+      .build()
     this.connection.on("ReceiveMessage", (messageString: string | Message) => {
-      this.onMessage(messageString);
-    });
+      this.onMessage(messageString)
+    })
     return this.connection
       .start()
       .then(() => {
-        this.connected = true;
+        this.connected = true
       })
       .catch((err: Error) => {
-        this.onError(err);
-      });
+        this.onError(err)
+      })
   }
 
   sendNetworkMessage(msg: Message) {
-    const msgJson = JSON.stringify(msg);
+    const msgJson = JSON.stringify(msg)
     this.connection?.invoke("SendMessage", msgJson).catch((err) => {
-      this.onError(err as Error);
-      return console.error(err.toString());
-    });
+      this.onError(err as Error)
+      return console.error(err.toString())
+    })
   }
 }
